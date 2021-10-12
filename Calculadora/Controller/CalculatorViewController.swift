4ad12141c223aeab7b158c00e3aea36dad2c9f34
Fernando Luiz Goulart <fernando@leapi.com>
2021-10-12 18:37:22 -0300

    Digits, operators and equals ok

--- Calculadora/Controller/CalculatorViewController.swift
+++ Calculadora/Controller/CalculatorViewController.swift
@@ -11,6 +11,7 @@ class CalculatorViewController: UIViewController {
 
     let mathOperation = MathOperation()
 
+    @IBOutlet weak var displayUILabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,10 +20,21 @@ class CalculatorViewController: UIViewController {
         guard let input = sender.titleLabel?.text else { return }
         let mathOperator = Character(input)
         guard (try? mathOperation.operatorInput(mathOperator)) != nil else { return }
+        updateDisplay()
     }
     @IBAction func digitUIButtonAction(_ sender: UIButton) {
         guard let input = sender.titleLabel?.text else { return }
         let digit = Character(input)
         guard (try? mathOperation.digitInput(digit)) != nil else { return }
+        updateDisplay()
+    }
+
+    @IBAction func equalsUIButton(_ sender: UIButton) {
+        mathOperation.equalsInput()
+        updateDisplay()
+    }
+
+    private func updateDisplay() {
+        displayUILabel.text = mathOperation.display
     }
 }
